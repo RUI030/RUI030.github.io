7c9c55bb6d1215d55c0c0c7f0c49f9690b39fd3d
RUI030 <61765695+RUI030@users.noreply.github.com>
2026-03-13 23:44:01 -0400

    fix bug of project cards and update project contents

--- js/projects.js
+++ js/projects.js
@@ -10,6 +10,8 @@ $(document).ready(
         $('.go-aiwc').on('click', function () { goTo('https://github.com/Terence1219/WeatherClassifierOnKV260');});        
         $('.go-l0deblur').on('click', function () { goTo('https://drive.google.com/file/d/1MmIHbRMFCbV_EQQUUTUJskwjj-UPB3Gm/view?usp=drive_link');});        
         $('.go-dml').on('click', function () { goTo('https://drive.google.com/file/d/1M0jTyTpKl-oYlQz1GXb2cshQBrmEiCHX/view?usp=drive_link');});        
+        $('.go-CircuitVideo').on('click', function () { goTo('https://www.youtube.com/watch?v=NS2VoYjIKHI');});        
+        $('.card a').on('click', function (e) {e.stopPropagation()}) // this make sure the hyper link in the description will not go to the JS linked card link above
         //scroll
         $('.to-software').on('click', function () { scrollTo('#software') });
         $('.to-hardware').on('click', function () { scrollTo('#hardware') });
